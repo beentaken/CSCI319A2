bc0a9bd61b68d01b0fdce08bebf8c02cc4686ead
mthomp40 <mthommo79@gmail.com>
2015-04-23 20:35:48 +1000

    Initial

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <cstdlib>
+#include <cmath>
 #include "ChordNode.h"
-#include "FingerTable.h"
+#include "FingerTableElement.h"
 using namespace std;
 
-FingerTable::FingerTable(int size) {
-    fingers = new int*[size];
-}
+ChordNode *FindSuccessor(ChordNode *node, int ID);
 
 ChordNode::ChordNode(int ID, int size) {
     this->ID = ID;
     this->size = size;
     this->data = new std::map<std::string, std::string>();
+    this->fingerTable = new FingerTableElement[size];
+    for (int i = 0; i < size; i++) {
+        if (ID == 0) {
+            fingerTable[i].interval = ID + pow(2, i);
+            fingerTable[i].successorID = 0;
+            fingerTable[i].successorNode = this;
+        }
+    }
 }
 
 void ChordNode::AddPeer(int ID) {
-
+    ChordNode *newnode = new ChordNode(ID, this->size);
+    for (int i = 0; i < size; i++) {
+        fingerTable[i].interval = ID + pow(2, i);
+        fingerTable[i].successorNode = FindSuccessor(this, fingerTable[i].interval);
+        fingerTable[i].successorID = fingerTable[i].successorNode->GetID();
+    }
+    Print(7);
 }
 
 void ChordNode::RemovePeer(int ID) {
@@ -29,12 +42,23 @@ void ChordNode::Delete(std::string data) {
 
 }
 
-ChordNode *ChordNode::FindPredecessor(int ID) {
+ChordNode *FindPredecessor(int ID) {
 
 }
 
-ChordNode *ChordNode::FindSuccessor(int ID) {
+ChordNode *FindSuccessor(ChordNode *node, int ID) {
+    for (int i = (node->GetSize() - 1); i >= 0; i--) {
+        if (node->GetFingerTable(i).interval < ID) {
+            cout << "Less than: " << i << "\n";
+            return node->GetFingerTable(i).successorNode;
+        } else if (node->GetFingerTable(i).interval == ID) {
+            cout << "Equals: " << i << "\n";
+            return node->GetFingerTable(i).successorNode;
+        } else {
+            cout << "Else: " << i << "\n";
+        }
 
+    }
 }
 
 int ChordNode::FindKey(std::string key) {
@@ -50,7 +74,22 @@ int ChordNode::Hash(std::string data) {
 }
 
 void ChordNode::Print(int ID) {
+    cout << "Int\tID\n";
+    for (int i = 0; i < 5; i++) {
+        cout << this->fingerTable[i].interval << "\t" << this->fingerTable[i].successorID << "\n";
+    }
+}
 
+int ChordNode::GetSize() {
+    return this->size;
+}
+
+int ChordNode::GetID() {
+    return this->ID;
+}
+
+FingerTableElement ChordNode::GetFingerTable(int index) {
+    return this->fingerTable[index];
 }
 
 ChordNode *InitChord(int size) {
